a395d17d3c773ac36fc37b0da15e0a4367301ff1
Vinícius Poloni Crisol <vinicius.crisol@hotmail.com>
2020-12-29 22:13:43 -0300

    Migration fix

--- swimsuits_api/priv/repo/migrations/20201230000902_add_states_table.exs
+++ swimsuits_api/priv/repo/migrations/20201230000902_add_states_table.exs
@@ -2,11 +2,11 @@ defmodule SwimsuitsApi.Repo.Migrations.AddUsersTable do
   use Ecto.Migration
 
   def change do
-    create table(:Users, primary_key: false) do
+    create table(:states, primary_key: false) do
       add :id, :uuid, primary_key: true
       add :abbreviation, :string, size: 2
       timestamps()
     end
-    create unique_index(:Users, [:abbreviation])
+    create unique_index(:states, [:abbreviation])
   end
 end
